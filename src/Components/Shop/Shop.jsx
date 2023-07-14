47710aa49c7190510c7eacf3b8b12ddf31c375d4
ARIF-630419 <ariful25-013@diu.edu.bd>
2023-07-14 05:36:55 -0500

    first commit

---
+++ src/Components/Shop/Shop.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./Shop.css";
+import { useEffect } from "react";
+import Product from "../Product/Product";
+const Shop = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    fetch("products.json")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+  const handleAddToCart = (product) => {
+    const newCart = [...cart, product];
+    setCart(newCart);
+  };
+
+  return (
+    <div className="shop-container">
+      <div className="product-container">
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            product={product}
+            handleAddToCart={handleAddToCart}
+          ></Product>
+        ))}
+      </div>
+      <div className="cart-container">
+        <h4>summary details</h4>
+        <p>selected item: {cart.length}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
